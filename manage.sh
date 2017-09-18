2a6c6f14738b569778742a77d6c1fd99d845a55c
Yuxiang LIU <david-khala@hotmail.com>
2017-09-18 18:55:42 +0800

    add swarm helper

--- manage.sh
+++ manage.sh
@@ -29,104 +29,101 @@ while getopts "v:" shortname $remain_params; do
 	esac
 done
 
-
-
 function clearContainer() {
-    local PATTERN=""
-    local IMPOSED_ARR=$(jq -r ".docker.containers.clearPattern.imposed[]" $CONFIG_JSON)
-    for IMPOSE_EACH in $IMPOSED_ARR; do
-        PATTERN="$PATTERN\|"$IMPOSE_EACH
-    done
-    PATTERN=${PATTERN:2} # substring
+	local PATTERN=""
+	local IMPOSED_ARR=$(jq -r ".docker.containers.clearPattern.imposed[]" $CONFIG_JSON)
+	for IMPOSE_EACH in $IMPOSED_ARR; do
+		PATTERN="$PATTERN\|"$IMPOSE_EACH
+	done
+	PATTERN=${PATTERN:2} # substring
 
-    local CONTAINER_IDS=$(docker ps -a | grep "$PATTERN" | awk '{print $1}')
+	local CONTAINER_IDS=$(docker ps -a | grep "$PATTERN" | awk '{print $1}')
 
-    utils/clear.sh container $CONTAINER_IDS
+	utils/clear.sh container $CONTAINER_IDS
+	docker container prune --force # delete all stopped container
 }
 
 function clearImage() {
-    local isFast=$1
-    local PATTERN=""
-    local IMPOSED_ARR=$(jq -r ".docker.images.clearPattern.imposed[]" $CONFIG_JSON)
-    for IMPOSE_EACH in $IMPOSED_ARR; do
-        PATTERN="$PATTERN\|"$IMPOSE_EACH
-    done
-    PATTERN=${PATTERN:2} # substring
+	local isFast=$1
+	local PATTERN=""
+	local IMPOSED_ARR=$(jq -r ".docker.images.clearPattern.imposed[]" $CONFIG_JSON)
+	for IMPOSE_EACH in $IMPOSED_ARR; do
+		PATTERN="$PATTERN\|"$IMPOSE_EACH
+	done
+	PATTERN=${PATTERN:2} # substring
 
-    if [ -z "$isFast" ]; then
-        local BASIC=""
-        local BASIC_ARR=$(jq -r ".docker.images.clearPattern.fundamental[]" $CONFIG_JSON)
-        for BASIC_EACH in $BASIC_ARR; do
-            BASIC="$BASIC\|"$BASIC_EACH
-        done
-        BASIC=${BASIC:2} # substring
-        if [ ! -z "$BASIC" ]; then
-          PATTERN="$PATTERN\|"$BASIC
-        fi
-    fi
-    DOCKER_IMAGE_IDS=$(docker images | grep "$PATTERN" | awk '{print $3}')
-    utils/clear.sh image $DOCKER_IMAGE_IDS
+	if [ -z "$isFast" ]; then
+		local BASIC=""
+		local BASIC_ARR=$(jq -r ".docker.images.clearPattern.fundamental[]" $CONFIG_JSON)
+		for BASIC_EACH in $BASIC_ARR; do
+			BASIC="$BASIC\|"$BASIC_EACH
+		done
+		BASIC=${BASIC:2} # substring
+		if [ ! -z "$BASIC" ]; then
+			PATTERN="$PATTERN\|"$BASIC
+		fi
+	fi
+	DOCKER_IMAGE_IDS=$(docker images | grep "$PATTERN" | awk '{print $3}')
+	utils/clear.sh image $DOCKER_IMAGE_IDS
 }
 
 function networkResume() {
-    callback/onResume.sh
-    utils/compose.sh up $COMPOSE_FILE
+	callback/onResume.sh
+	utils/compose.sh up $COMPOSE_FILE
 
-    echo ===resume finished:
-    utils/docker.sh view
-    callback/onRefresh.sh
+	echo ===resume finished:
+	utils/docker.sh view
+	callback/onRefresh.sh
 }
 function networkUp() {
-    callback/onUp.sh
+	callback/onUp.sh
 
-    PULL_IMAGES=$(jq -r '.docker.images.pull[]' $CONFIG_JSON)
-    for pullImage in $PULL_IMAGES;do
-        utils/docker.sh pull $pullImage:$IMAGE_TAG
-    done
+	PULL_IMAGES=$(jq -r '.docker.images.pull[]' $CONFIG_JSON)
+	for pullImage in $PULL_IMAGES; do
+		utils/docker.sh pull $pullImage:$IMAGE_TAG
+	done
 
-    networkResume
-    echo ===up finished:
-    utils/docker.sh view
+	networkResume
+	echo ===up finished:
+	utils/docker.sh view
 }
 
 function networkPause() {
-    utils/compose.sh down $COMPOSE_FILE
-    clearContainer
-    clearImage fast
+	utils/compose.sh down $COMPOSE_FILE
+	clearContainer
+	clearImage fast
 
-    callback/onPause.sh
-    echo ===pause finished:
-    utils/docker.sh view
+	callback/onPause.sh
+	echo ===pause finished:
+	utils/docker.sh view
 
 }
 function networkDown() {
-    networkPause
-    clearImage
+	networkPause
+	clearImage
 
-    callback/onDown.sh
-    echo ===down finished:
-    utils/docker.sh view
+	callback/onDown.sh
+	echo ===down finished:
+	utils/docker.sh view
 }
 
-
-
 if [ "${UP_DOWN}" == "up" ]; then
-    networkUp
+	networkUp
 elif [ "${UP_DOWN}" == "down" ]; then
-    ## Clear the network
-    networkDown
+	## Clear the network
+	networkDown
 elif [ "${UP_DOWN}" == "restart" ]; then
-    ## Restart the network
-    networkDown
-    networkUp
+	## Restart the network
+	networkDown
+	networkUp
 elif [ "${UP_DOWN}" == "pause" ]; then
-    networkPause
+	networkPause
 elif [ "${UP_DOWN}" == "resume" ]; then
-    networkPause
-    networkResume
+	networkPause
+	networkResume
 elif [ "${UP_DOWN}" == "refresh" ]; then
-    callback/onRefresh.sh
+	callback/onRefresh.sh
 else
-    callback/help.sh
-    exit 1
+	callback/help.sh
+	exit 1
 fi
---
+++ utils/swarm.sh
@@ -0,0 +1,32 @@
+fcn="$1"
+remain_params=""
+for ((i = 2; i <= $#; i++)); do
+	j=${!i}
+	remain_params="$remain_params $j"
+done
+function viewService() {
+	docker node ps "$1" # default to view current node
+}
+function viewNode() {
+	local isPretty="--pretty"
+	if [ ! "$2" == "$isPretty" ]; then
+		isPretty=""
+	fi
+	echo $isPretty
+	if [ -z "$1" ]; then
+		docker node inspect self $isPretty
+	else
+		docker node inspect "$1" $isPretty
+	fi
+}
+function viewSwarm() {
+	docker node ls
+}
+function getNodeID() {
+	local hostName="$1"
+	viewNode "$hostName" --pretty | grep "ID" | awk '{print $2}'
+}
+function getNodeIP() {
+	echo $(viewNode "$1") | jq -r ".[0].Status.Addr"
+}
+$fcn $remain_params
