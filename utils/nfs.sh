3ccb8ed0fec8657a474cddb2eed7d0af4fc9e0b8
Yuxiang LIU <david-khala@hotmail.com>
2017-09-26 23:03:04 +0800

    fix nfs

--- utils/nfs.sh
+++ utils/nfs.sh
@@ -4,7 +4,7 @@ nfsIP=$2
 nfsDIR=$3
 localDIR=$4
 setting="nfs rsize=8192,wsize=8192,timeo=14,intr"
-fstab="/etc/fstab/"
+fstab="/etc/fstab"
 function add() {
 	if ! grep $localDIR $fstab; then
 		echo "$nfsIP:$nfsDIR $localDIR $setting" | sudo tee -a $fstab
