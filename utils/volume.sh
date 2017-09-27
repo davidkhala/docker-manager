a86b1a1ae2bd97622b9a88b69534e2084d4e0a77
Yuxiang LIU <david-khala@hotmail.com>
2017-09-27 10:41:19 +0800

    add volume shell util

--- utils/volume.sh
+++ utils/volume.sh
@@ -6,7 +6,9 @@ for ((i = 2; i <= "$#"; i++)); do
 	remain_params="$remain_params $j"
 done
 function createLocal() {
-	docker volume create --name "$1" --opt o=bind --opt device="$2" --opt type=none
+	local name=$1
+	local path=$2
+	docker volume create --name $name --opt o=bind --opt device=$path --opt type=none
 }
 function delete() {
 	docker volume rm --force $1
