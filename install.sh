5cca81b6e73fccd555553c1b74fa84d55c7049eb
Yuxiang LIU <david-khala@hotmail.com>
2017-10-03 17:37:55 +0800

    try fix unauthenticated packages when install docker ce

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # install docker-ce
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
@@ -12,7 +16,7 @@ sudo gpasswd -a $USER docker
 newgrp docker
 
 # install docker-compose
-sudo su - -c "curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo su - -c "curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-$(uname -s)-$(uname -m) > /usr/local/bin/docker-compose"
 sudo su - -c "chmod +x /usr/local/bin/docker-compose"
 
 # install jq for parsing json content
