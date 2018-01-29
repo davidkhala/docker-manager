d550998261fb500b0eb2eba9a39b85177cc1f0b5
davidliu <david-khala@hotmail.com>
2018-01-29 15:10:37 +0800

    fix sudo problem

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -3,15 +3,12 @@
 
 fcn="$1"
 function cn(){
-    # install nodejs
-    curl -sL https://deb.nodesource.com/setup_6.x | bash -
-    apt-get -qq install -y nodejs
     npm config set registry https://registry.npm.taobao.org/
 }
 if [ -n "$fcn" ];then
     $fcn
-else
-    # install nodejs
-    curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-    apt-get -qq install -y nodejs
 fi
+
+# install nodejs
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get -qq install -y nodejs
