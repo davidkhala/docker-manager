f9f46a142de235f215485120e2952c897fc52078
davidliu <david-khala@hotmail.com>
2018-03-06 19:41:25 +0800

    try submodule update

--- install.sh
+++ install.sh
@@ -65,11 +65,9 @@ function default() {
 	sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 	sudo apt-key fingerprint 0EBFCD88
-	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 	sudo apt-get update
-	sudo apt-get -qq install -y docker-ce=$dockerVersion
+	sudo apt-get -qq install -y --allow-downgrades docker-ce=$dockerVersion
 
     jq
 	curl -L https://github.com/docker/compose/releases/download/${composeVersion}/docker-compose-$(uname -s)-$(uname -m) >/usr/local/bin/docker-compose
