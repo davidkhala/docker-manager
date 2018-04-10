fc62104c92ca44d6e37f65f7ce59c7f986a5df62
davidliu <david-khala@hotmail.com>
2018-04-09 23:27:25 -0700

    check before install

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-
 set -e
 fcn="$1"
-function cn(){
-    npm config set registry https://registry.npm.taobao.org/
+function cn() {
+	npm config set registry https://registry.npm.taobao.org/
 }
-if [ -n "$fcn" ];then
-    $fcn
+if [ -n "$fcn" ]; then
+	$fcn
 fi
-
-# install nodejs
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt-get -qq install -y nodejs
+if ! node --version | grep 'v8.'; then
+	# install nodejs
+	curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+	sudo apt-get -qq install -y nodejs
+fi
