bdb0ee9ada5ee69a1aa314f896e58ac0f760d54b
davidliu <david-khala@hotmail.com>
2018-08-15 23:33:23 +0800

    add fcn to change package-lock

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
-CURRENT=$(cd $(dirname ${BASH_SOURCE}); pwd)
+CURRENT=$(
+	cd $(dirname ${BASH_SOURCE})
+	pwd
+)
 set -e
-fcn="$1"
+fcn=$1
+remain_params=""
+for ((i = 2; i <= ${#}; i++)); do
+	j=${!i}
+	remain_params="$remain_params $j"
+done
+function packageLock() {
+	local CMD="npm config set package-lock $1"
+	echo $CMD
+	$CMD
+}
 if [ -n "$fcn" ]; then
-	$fcn
-fi
-if ! node --version | grep 'v8.'; then
-	# install nodejs
-	curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-	sudo apt-get -qq install -y nodejs
-	npm config set package-lock false
+	$fcn $remain_params
+else
+	if ! node --version | grep 'v8.'; then
+		# install nodejs
+		curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+		sudo apt-get -qq install -y nodejs
+	fi
 fi
