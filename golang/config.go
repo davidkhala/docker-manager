28103897d768d54dbb60e4289de1c0853d197bab
davidliu <david-khala@hotmail.com>
2018-08-30 14:13:54 +0800

    test failed and back up

--- golang/config.go
+++ golang/config.go
@@ -1,1 +1,83 @@
 package dockersdk
+
+import (
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	"strconv"
+	. "github.com/davidkhala/goutils"
+)
+
+// this is a mimic of nat.portSet
+type portSet map[string]struct{}
+
+// this is a mimic of container.Config
+type containerConfig struct {
+	container.Config
+	ExposedPorts portSet
+}
+
+func BuildContainerConfig(image string, Env []string, Cmd []string, volumes map[string]string, portMap map[int]int) (config *container.Config, hostConfig *container.HostConfig, networkingConfig *network.NetworkingConfig) {
+
+	var portSet = portSet{}
+	for k := range portMap {
+		portSet[strconv.Itoa(k)] = struct{}{}
+	}
+	var Volumes = map[string]struct{}{}
+	var Binds []string
+	for k, v := range volumes {
+		Volumes[v] = struct{}{}
+		Binds = append(Binds, k+":"+v)
+	}
+
+	var mimicConfig = containerConfig{
+		container.Config{
+			Image:   image,
+			Env:     Env,
+			Cmd:     Cmd,
+			Volumes: Volumes,
+		},
+		portSet,
+	}
+	var jsonTemp = ToJson(mimicConfig)
+	FromJson(jsonTemp, &config)
+	hostConfig = &container.HostConfig{
+		Binds:Binds,
+		PortBindings:
+	}
+}
+
+//
+//name: container_name,
+//Env,
+//Volumes: {
+//[peerUtil.container.MSPROOT]: {},
+//[ordererUtil.container.CONFIGTX]: {},
+//[ordererUtil.container.state]: {}
+//},
+//Cmd,
+//Image,
+//ExposedPorts: {
+//'7050': {},
+//},
+//Hostconfig: {
+//Binds: [
+//`MSPROOT:${peerUtil.container.MSPROOT}`,
+//`CONFIGTX:${ordererUtil.container.CONFIGTX}`,
+//`ledger:${ordererUtil.container.state}`
+//],
+//PortBindings: {
+//'7050': [
+//{
+//HostPort: '9050'
+//						}
+//]
+//},
+//},
+//NetworkingConfig: {
+//EndpointsConfig: {
+//[network]: {
+//Aliases: [container_name]
+//}
+//}
+//}
+//config *container.Config, hostConfig *container.HostConfig, networkingConfig *network.NetworkingConfig, containerName string
--- golang/dockerSDK.go
+++ golang/dockerSDK.go
@@ -4,9 +4,13 @@ import (
 	"github.com/docker/docker/client"
 	"context"
 	"github.com/docker/docker/api/types"
-	"github.com/davidkhala/delphi-fabric/common/docker/golang/golang"
+	. "github.com/davidkhala/goutils"
 	"github.com/docker/docker/api/types/filters"
-)
+	"fmt"
+	"strings"
+	"io"
+	"os"
+	)
 
 type Docker struct {
 	client  *client.Client
@@ -15,53 +19,100 @@ type Docker struct {
 
 func GetClient() Docker {
 	_client, err := client.NewEnvClient()
-	golang.PanicError(err)
+	PanicError(err)
 	return Docker{_client, context.Background()}
 }
 func (docker Docker) ContainerList() []types.Container {
 	containers, err := docker.client.ContainerList(docker.context, types.ContainerListOptions{})
-	golang.PanicError(err)
+	PanicError(err)
 	return containers
 }
 func (docker Docker) ImageList() []types.ImageSummary {
 	var listOpts = types.ImageListOptions{All: false}
 	var imageSummary, err = docker.client.ImageList(docker.context, listOpts)
-	golang.PanicError(err)
+	PanicError(err)
 	return imageSummary
 }
-func (docker Docker) ImageInspect(imageID string) (*types.ImageInspect) {
+func (docker Docker) ImageExist(imageID string) (exist bool) {
+	defer func() {
+		if err := recover(); err != nil {
+			var errString = err.(error).Error()
+			if strings.Contains(errString, "No such image: ") {
+				fmt.Println(errString)
+				exist = false
+			} else {
+				panic(err)
+			}
+		}
+	}()
+	docker.ImageInspect(imageID)
+	exist = true
+	return
+}
+func (docker Docker) ImageInspect(imageID string) (types.ImageInspect) {
 	info, _, err := docker.client.ImageInspectWithRaw(docker.context, imageID) //ignore the raw, which is replica of info
-	golang.PanicError(err)
-	if info.ID == "" {
-		return nil
-	}
-	return &info
+	PanicError(err)
+	return info
 }
 func (docker Docker) ImagePrune() types.ImagesPruneReport {
 	imagesPruneReport, err := docker.client.ImagesPrune(docker.context, filters.Args{})
-	golang.PanicError(err)
+	PanicError(err)
 	return imagesPruneReport
 }
 
 func (docker Docker) ImagePull(imageName string, force bool) {
 	var opts = types.ImagePullOptions{All: false}
 	if !force {
-		var exist = docker.ImageInspect(imageName)
-		if exist != nil {
+		var exist = docker.ImageExist(imageName)
+		if exist {
+			fmt.Println("image " + imageName + " already exist")
 			return
 		}
 	}
-	docker.client.ImagePull(docker.context, imageName, opts)
+	fmt.Println("start image pull " + imageName)
+	reader, err := docker.client.ImagePull(docker.context, imageName, opts)
+	PanicError(err)
+	defer reader.Close()
+	io.Copy(os.Stdout, reader)
 }
-func (docker Docker) ContainerCreate(containerName string, Env []string, Cmd []string) string {
-	config := types.ExecConfig{Env: Env, Cmd: Cmd}
+
+func (docker Docker) ContainerCreate(config *ContainerConfig) string {
 	var idResponse types.IDResponse
-	idResponse, err := docker.client.ContainerExecCreate(docker.context, containerName, config)
-	golang.PanicError(err)
+	idResponse, err := docker.client.ContainerCreate(docker.context, config, config)
+	//config *container.Config, hostConfig *container.HostConfig, networkingConfig *network.NetworkingConfig, containerName string
+	PanicError(err)
 	return idResponse.ID
 }
+//resp, err := cli.ContainerCreate(ctx, &container.Config{
+//Image: "alpine",
+//Cmd:   []string{"echo", "hello world"},
+//Tty:   true,
+//}, nil, nil, "")
+//if err != nil {
+//panic(err)
+//}
+//
+//if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+//panic(err)
+//}
+//
+//statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+//select {
+//case err := <-errCh:
+//if err != nil {
+//panic(err)
+//}
+//case <-statusCh:
+//}
+//
+//out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+//if err != nil {
+//panic(err)
+//}
+//
+//io.Copy(os.Stdout, out)
 func (docker Docker) ContainerDelete(containerID string) {
 	options := types.ContainerRemoveOptions{false, false, true}
 	err := docker.client.ContainerRemove(docker.context, containerID, options)
-	golang.PanicError(err)
+	PanicError(err)
 }
