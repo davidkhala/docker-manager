46086c3b5fef19536a1dd74d102814fe5f42064b
davidLiu <david-khala@hotmail.com>
2019-01-30 15:03:48 +0800

    double bracket

--- dockerSUDO.sh
+++ dockerSUDO.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # NOTE PLEASE run this without 'sudo', otherwise ENV $USER will be 'root' instead of current user.
 osVersion=`uname -s`
-if [ $osVersion != "Darwin" ]; then
+if [[ $osVersion != "Darwin" ]]; then
     sudo gpasswd -a $USER docker
 # NOTE newgrp starts a subshell with the group you specified. So that line in your script will not finish until that subshell is done.
     newgrp docker
