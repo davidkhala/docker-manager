020c4ff03d5086c5423488c5c159a0f0989c2482
Yuxiang LIU <davidkhala@users.noreply.github.com>
2019-08-05 13:14:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ for ((i = 2; i <= ${#}; i++)); do
 	remain_params="$remain_params $j"
 done
 
-dockerVersion=17.06.2
+dockerVersion=17.06
 composeVersion=1.14.0
 jqVersion=1.5
 while getopts "d:c:j:" shortname ${remain_params}; do
