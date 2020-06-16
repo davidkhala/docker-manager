cc561e3bd211ac0c4ed24c8d2a2a42e3d9f6079b
Yuxiang LIU <david-khala@hotmail.com>
2020-06-16 14:04:29 +0800

    Update dockerSUDO.sh
    Revert back. 
    According to https://docs.docker.com/engine/install/linux-postinstall/

--- dockerSUDO.sh
+++ dockerSUDO.sh
@@ -2,5 +2,14 @@
 # NOTE PLEASE run this without 'sudo', otherwise ENV $USER will be 'root' instead of current user.
 osVersion=`uname -s`
 if [[ $osVersion != "Darwin" ]]; then
-    sudo chmod 666 /var/run/docker.sock
+#     sudo chmod 666 /var/run/docker.sock
+    
+    
+    ## Reference: https://docs.docker.com/engine/install/linux-postinstall/
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+    newgrp docker
+    ## Reference: https://docs.docker.com/engine/install/linux-postinstall/
+    
+    
 fi
