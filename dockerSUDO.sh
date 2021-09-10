a0dbbdab38d2735e3bc8fe07c33bacbc90ed7a3b
David Liu <david.yx.liu@oracle.com>
2021-09-10 04:23:12 -0400

    Update dockerSUDO.sh

--- dockerSUDO.sh
+++ dockerSUDO.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # NOTE PLEASE run this without 'sudo', otherwise ENV $USER will be 'root' instead of current user.
 echo "See in https://docs.docker.com/engine/security/rootless/"
+curl -fsSL https://get.docker.com/rootless | sh
