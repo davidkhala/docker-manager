9efd31da685f51d15f050c2dc20870a802cb9557
David Liu <david.yx.liu@oracle.com>
2021-09-10 04:20:14 -0400

    Update dockerSUDO.sh

--- dockerSUDO.sh
+++ dockerSUDO.sh
@@ -1,13 +1,3 @@
 #!/usr/bin/env bash
 # NOTE PLEASE run this without 'sudo', otherwise ENV $USER will be 'root' instead of current user.
-osVersion=`uname -s`
-if [[ $osVersion != "Darwin" ]]; then
-#     sudo chmod 666 /var/run/docker.sock
-    
-    
-    ## Reference: https://docs.docker.com/engine/install/linux-postinstall/
-    sudo groupadd docker
-    sudo usermod -aG docker $USER
-    newgrp docker
-    
-fi
+echo "See in https://docs.docker.com/engine/security/rootless/"
