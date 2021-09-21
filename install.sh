d32427eb9756ef1b3da46b8de40436d41558ba79
DavidLiu <david.yx.liu@oracle.com>
2021-09-21 09:18:54 +0800

    squash

--- install.sh
+++ install.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env bash
 set -e
 ## Note: below is for reference only, please switch to use platform specific install tools
-
-shipyard() {
-	local action=$1
-	if [[ ${action} == "install" ]]; then
-		curl -sSL https://shipyard-project.com/deploy | sudo bash -s
-	elif [[ ${action} == "uninstall" ]]; then
-		curl -sSL https://shipyard-project.com/deploy | ACTION=remove sudo -E bash -s cause -E preserves environmental vaiables set
-	elif [[ ${action} == "refresh" ]]; then
-		curl -sSL https://shipyard-project.com/deploy | ACTION=upgrade sudo bash -s
-	fi
+diagnose() {
+	ls -al /var/run/docker.sock
 }
+
 Docker() {
 	curl -sSL https://get.docker.com/ | sh
-
 }
 
 $@
