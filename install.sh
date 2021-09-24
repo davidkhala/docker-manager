35e78d8845399b01703726e9aa992bef7282751b
David Liu <david.yx.liu@oracle.com>
2021-09-24 00:23:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ system-service() {
 	# On Debian and Ubuntu, the Docker service is configured to start on boot by default. 
 	sudo systemctl enable docker.service
  	sudo systemctl enable containerd.service
+	sudo systemctl start docker.service
 }
 Docker() {
 	curl -sSL https://get.docker.com/ | sh
