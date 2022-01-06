473d76e12c64561c1fc80caf18066b8e5737745b
DavidLiu <david.yx.liu@oracle.com>
2022-01-06 13:56:31 +0800

    [Done]  docker push to ocir

--- login.sh
+++ login.sh
@@ -5,9 +5,9 @@ ocr() {
     docker login container-registry.oracle.com
 }
 ocir() {
-    local region_key=$1
-    local tenancy_namespace=$2
-    local username=$3
+    local region_key=${1:-'ap-seoul-1'}
+    local tenancy_namespace=${2:-cn9yc2hk0gzg}
+    local username=${3:-'davidkhala@gmail.com'}
     echo Please use auth_token as password...
     docker login $region_key.ocir.io --username $tenancy_namespace/$username
 
--- push.sh
+++ push.sh
@@ -1,8 +1,10 @@
 ocir() {
     # <region-key>.ocir.io/<tenancy-namespace>/<repo-name>:<tag>
-    local region_key=$1
-    local tenancy_namespace=$2
-    local image=$3
+    
+    local tenancy_namespace=${3:-cn9yc2hk0gzg}
+    local region_key=${2:-'ap-seoul-1'}
+    local image=$1
+
     docker tag $image $region_key.ocir.io/$tenancy_namespace/$image
 
     docker push $region_key.ocir.io/$tenancy_namespace/$image
