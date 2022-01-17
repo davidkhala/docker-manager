7b6b246bef695c08fe4d83c1e71b9690c32f0fd5
DavidLiu <david.yx.liu@oracle.com>
2022-01-17 22:19:04 +0800

    special render for OCI

--- login.sh
+++ login.sh
@@ -5,9 +5,10 @@ ocr() {
     docker login container-registry.oracle.com
 }
 ocir() {
-    local region_key=${1:-'ap-seoul-1'}
-    local tenancy_namespace=${2:-cn9yc2hk0gzg}
-    local username=${3:-'davidkhala@gmail.com'}
+    local username=${1:-'davidkhala@gmail.com'}
+    local region_key=${2:-'ap-seoul-1'}
+    local tenancy_namespace=${3:-cn9yc2hk0gzg}
+    
     echo Please use auth_token as password...
     docker login $region_key.ocir.io --username $tenancy_namespace/$username
 
--- push.sh
+++ push.sh
@@ -1,5 +1,7 @@
 ocir() {
-    # <region-key>.ocir.io/<tenancy-namespace>/<repo-name>:<tag>
+    # Usually, before pushing images to a repository, you'll first create an empty repository in a compartment. The name you give the repository must be unique across all compartments in the entire tenancy.
+    # Once there is an existing repository within a compartment, since it it globally unique in tenancy, the same docker push will auto-targetting to compartment than root-compartment
+
     
     local tenancy_namespace=${3:-cn9yc2hk0gzg}
     local region_key=${2:-'ap-seoul-1'}
