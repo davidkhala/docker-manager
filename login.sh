69474e2c37648c6ead4ab5940d8b99e425c467bd
DavidLiu <david.yx.liu@oracle.com>
2022-04-12 22:53:58 +0800

    ocir reformat

--- login.sh
+++ login.sh
@@ -1,16 +1,16 @@
 docker-hub() {
-    docker login registry.hub.docker.com
+	docker login registry.hub.docker.com
 }
 ocr() {
-    docker login container-registry.oracle.com
+	docker login container-registry.oracle.com
 }
 ocir() {
-    local username=${1:-'davidkhala@gmail.com'}
-    local region_key=${2:-'ap-seoul-1'}
-    local tenancy_namespace=${3:-cn9yc2hk0gzg}
-    
-    echo Please use auth_token as password...
-    docker login $region_key.ocir.io --username $tenancy_namespace/$username
+	local username=${username:-'davidkhala@gmail.com'}
+	local tenancy_namespace=${tenancy_namespace:-cn9yc2hk0gzg}
+	local region_key=${region_key:-'ap-singapore-1'}
+
+	echo Please use auth_token as password...
+	docker login $region_key.ocir.io --username $tenancy_namespace/$username
 
 }
 $@
--- push.sh
+++ push.sh
@@ -1,16 +1,17 @@
 ocir() {
-    # Usually, before pushing images to a repository, you'll first create an empty repository in a compartment. The name you give the repository must be unique across all compartments in the entire tenancy.
-    # Once there is an existing repository within a compartment, since it it globally unique in tenancy, the same docker push will auto-targetting to compartment than root-compartment
+    # Usually, before pushing images to a repository, you'll first create an empty repository in a compartment.
+    # The name you give the repository must be unique across all compartments in the entire tenancy(globally).
+    # The same docker push will auto-targeting to compartment than root-compartment
 
-    
+
     local tenancy_namespace=${3:-cn9yc2hk0gzg}
-    local region_key=${2:-'ap-seoul-1'}
+    local region_key=${2:-'ap-singapore-1'}
     local image=$1
 
     docker tag $image $region_key.ocir.io/$tenancy_namespace/$image
 
     docker push $region_key.ocir.io/$tenancy_namespace/$image
-    docker image rm --no-prune $region_key.ocir.io/$tenancy_namespace/$image 
+    docker image rm --no-prune $region_key.ocir.io/$tenancy_namespace/$image
 }
 
 $@
