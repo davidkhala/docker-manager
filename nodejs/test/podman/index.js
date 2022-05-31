2968f691c95e421192f9d53446dde4e56a811657
DavidLiu <david.yx.liu@oracle.com>
2022-05-31 13:14:42 +0800

    [DONE] podman ping

--- nodejs/test/podman/index.js
+++ nodejs/test/podman/index.js
@@ -1,10 +1,18 @@
-const DockerManager = require('../../docker');
-const assert = require('assert')
-
+import DockerManager from '../../docker.js';
+import assert from 'assert'
+import os from 'os'
 describe('remote docker', () => {
 
+	it('show uid', async () => {
+		const userInfo = os.userInfo();
+
+		const uid = userInfo.uid;
+		console.debug({uid})
+	})
 	it('podman', async () => {
-		const socketPath = '/run/podman/podman.sock'
+		const userInfo = os.userInfo();
+
+		const socketPath = `/run/user/${userInfo.uid}/podman/podman.sock`
 		const docker = new DockerManager({socketPath})
 		const info = await docker.ping();
 		assert.strictEqual(info, "OK")
