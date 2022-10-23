93df840832a20782fcf31df6ac29008070dd6c71
David Liu <david.yx.liu@oracle.com>
2022-10-23 10:52:52 +0800

    Update install.sh

--- docker-compose/install.sh
+++ docker-compose/install.sh
@@ -1,7 +1,8 @@
-manually() {
+linux-manually() {
 	DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 	mkdir -p $DOCKER_CONFIG/cli-plugins
 	curl -SL https://github.com/docker/compose/releases/download/v2.12.1/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
 	sudo chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 }
+
 $@
