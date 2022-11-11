937459e96db15aa24f3f4dca5e02315198488e6f
David Liu <david.yx.liu@oracle.com>
2022-11-11 11:34:15 +0000

    add docker hub push

--- push.sh
+++ push.sh
@@ -3,27 +3,26 @@ ocir() {
     # The name you give the repository must be unique across all compartments in the entire tenancy(globally).
     # The same docker push will auto-targeting to compartment than root-compartment
 
-
     local tenancy_namespace=${tenancy_namespace:-cn9yc2hk0gzg}
     local region_key=${region_key:-'ap-singapore-1'}
     local image=$1
 
-    docker tag $image $region_key.ocir.io/$tenancy_namespace/$image
-
-    docker push $region_key.ocir.io/$tenancy_namespace/$image
-    docker image rm --no-prune $region_key.ocir.io/$tenancy_namespace/$image
+    docker-hub $region_key.ocir.io/$tenancy_namespace $image
 }
 
-aws-public(){
-	## aws ecr-public
+docker-hub() {
+    local namespace=${1:-davidkhala}
+    local image=$2
+    docker tag $image $namespace/$image
+    docker push $namespace/$image
+    docker image rm --no-prune $namespace/$image
+}
+
+aws-public() {
+    ## aws ecr-public
     local tenancy_namespace=${tenancy_namespace:-davidkhala}
     local image=$1
-    
-    docker tag $image public.ecr.aws/$tenancy_namespace/$image
-    
-    docker push public.ecr.aws/$tenancy_namespace/$image
-    docker image rm --no-prune public.ecr.aws/$tenancy_namespace/$image
+    docker-hub public.ecr.aws/$tenancy_namespace $image
 }
-
 
 $@
