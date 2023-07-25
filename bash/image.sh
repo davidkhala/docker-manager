e448a86bdeef3b083fceeaa4f264fe5c70d2f76a
David Liu <david-khala@hotmail.com>
2023-07-25 08:53:20 +0800

    Update image.sh

--- bash/image.sh
+++ bash/image.sh
@@ -10,4 +10,13 @@ trim() {
 	echo    WARNING! This will remove all images without at least one container associated to them.
 	docker image prune -a
 }
+
+build() {
+	local imageName=$1
+	local buildContext=${2:-.}
+	# --progress=plain --no-cache: to display full output during build
+	# See in: https://stackoverflow.com/questions/52915701/displaying-help-messages-while-docker-build
+	docker build --tag="$imageName" --progress=plain --no-cache "$buildContext" 
+}
+
 $@
