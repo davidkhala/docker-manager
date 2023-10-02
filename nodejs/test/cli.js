2c14d64e1f816c386dd8d37c5ef32fd4b39a3296
David Liu <david-khala@hotmail.com>
2023-10-02 14:08:49 +0800

    backup

--- nodejs/test/cli.js
+++ nodejs/test/cli.js
@@ -1,14 +1,14 @@
-import {daemonJsonFile, setHosts} from "../dockerCmd.js";
-import fs from "fs";
+import {daemonJsonFile, setHosts} from '../dockerCmd.js';
+import fs from 'fs';
 
 describe('docker cli', function () {
-	this.timeout(0)
+	this.timeout(0);
 	if (!process.env.CI) {
 		it('set Hosts', () => {
-			setHosts()
-			const content = fs.readFileSync(daemonJsonFile).toString()
-			console.info(content)
-		})
+			setHosts();
+			const content = fs.readFileSync(daemonJsonFile).toString();
+			console.info(content);
+		});
 	}
 
-})
+});
