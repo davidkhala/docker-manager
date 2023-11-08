4c23ef973538e1144a74c43533ab1751b0163ff2
David Liu <david-khala@hotmail.com>
2023-11-09 06:32:55 +0800

    Update run.sh

--- bash/run.sh
+++ bash/run.sh
@@ -6,5 +6,6 @@ if [[ -n "$Name" ]]; then
     docker run -d -p=${hostPort}:${containerPort} --name=${Name} $@ $image
     echo $Name
 else
+    # Ref: https://stackoverflow.com/questions/71083248/how-to-return-random-generated-name-instead-of-id-when-starting-docker-container
     docker run -d -p=${hostPort}:${containerPort} $@ $image | xargs docker container inspect -f '{{ slice .Name 1 }}'
 fi
