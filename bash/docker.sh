f4a5b37429177ea6d21b98691e4ace07e42b4502
David Liu <david-khala@hotmail.com>
2023-11-11 09:31:28 +0800

    Update docker.sh

--- bash/docker.sh
+++ bash/docker.sh
@@ -3,9 +3,9 @@ host-info(){
 	docker system info --format '{{.OSType}}/{{.Architecture}}'
 }
 
-rootless(){
+rootless-install(){
   # NOTE PLEASE run this without 'sudo', otherwise ENV $USER will be 'root' instead of current user.
-  echo "See in https://docs.docker.com/engine/security/rootless/"
+  # See in https://docs.docker.com/engine/security/rootless/
   curl -fsSL https://get.docker.com/rootless | sh
   export DOCKER_HOST=unix:///run/user/$UID/docker.sock
 }
